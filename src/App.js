455246f90ddc4dde2631b8d44e5cf2d8153dd8f9
Tatiana Torbanenko <tatianatorbanenko@Tatianas-MacBook-Air.local>
2023-10-10 19:09:17 -0700

    Lesson_1_1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Test 1'
+  },
+  {
+    id: 2,
+    title: 'Test 2'
+  },
+  {
+    id: 3,
+    title: 'Test 3'
+  }
+]
+
 function App() {
   return (
     <div>
       <h1> Todo List </h1>
       <ul>
-        <li></li>
-        <li></li>
+        {todoList.map(function(item) {
+          return <li key = {item.id}>
+            {item.title}
+            </li>;
+        })}      
       </ul>
-
-
     </div>
   );
 }
-
 export default App;
